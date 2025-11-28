d28f07605d26d8a2d336a7acba1a538ff45bf66b
BarisElvanoglu <elvanoglubaris12@gmail.com>
2025-11-28 18:05:32 +0300

    Product ve category entityleri oluşturuldu.

---
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+    /// <summary>
+    /// Category entity
+    /// </summary>
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryID { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
