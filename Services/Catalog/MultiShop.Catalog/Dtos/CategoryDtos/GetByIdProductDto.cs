e1a70c9b96b4ddcefa6ff015317b8059be935128
BarisElvanoglu <elvanoglubaris12@gmail.com>
2025-12-01 12:46:01 +0300

    Product ve Category Dto lar

---
+++ Services/Catalog/MultiShop.Catalog/Dtos/CategoryDtos/GetByIdProductDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MultiShop.Catalog.Dtos.CategoryDtos
+{
+    public class GetByIdProductDto
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductStock { get; set; }
+        public int ProductImageUrl { get; set; }
+        public string ProductDescriptipn { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
---
+++ Services/Catalog/MultiShop.Catalog/Dtos/PruductDtos/CreatePruductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiShop.Catalog.Dtos.PruductDtos
+{
+    public class CreatePruductDto
+    {
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductStock { get; set; }
+        public int ProductImageUrl { get; set; }
+        public string ProductDescriptipn { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
