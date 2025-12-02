f8db4b7653f6a664f4b0b5acc77a18ba7914a613
BarisElvanoglu <elvanoglubaris12@gmail.com>
2025-12-02 12:39:38 +0300

    Category Service ctor methodu

--- Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
+++ Services/Catalog/MultiShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -2,13 +2,24 @@
 using MongoDB.Driver;
 using MultiShop.Catalog.Dtos.CategoryDtos;
 using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Settings;
 
 namespace MultiShop.Catalog.Services.CategoryServices
 {
     public class CategoryService : ICategoryService
     {
-        private readonly  IMongoCollection<Category> _CategoryCollectin;
+        
+        private readonly  IMongoCollection<Category> _categoryCollection;
         private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper,IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _categoryCollection = database.GetCollection<Category>(_databaseSettings.CategoryConnectionName);
+            _mapper = mapper;
+        }
+
         public Task CreateCategoryAsync(CreateProdctDto createCategoryDto)
         {
             throw new NotImplementedException();
