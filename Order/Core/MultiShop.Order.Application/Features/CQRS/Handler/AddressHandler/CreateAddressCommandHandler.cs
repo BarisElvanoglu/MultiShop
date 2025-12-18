9e9e3b05e85597279fdad4f472261db37584279a
BarisElvanoglu <elvanoglubaris12@gmail.com>
2025-12-18 14:50:05 +0300

    Commandlar ve Create Adres command Handler

---
+++ Order/Core/MultiShop.Order.Application/Features/CQRS/Handler/AddressHandler/CreateAddressCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MultiShop.Order.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiShot.Order.Applicatinon.Entities;
+using MultiShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handler.AddressHandler
+{
+    public class CreateAddressCommandHandler
+    {
+        private readonly IRepository<Address> _addressRepository;   
+        
+        public CreateAddressCommandHandler(IRepository<Address> addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
+        
+        public async Task Handle(CreateAddressCommand address)
+        {
+           
+            await _addressRepository.CreateAsync(new Address
+            {
+                UserId = address.UserId,
+                City = address.City,
+                District = address.District,
+                Detail = address.Detail
+            }); 
+        }
+    }
+}
