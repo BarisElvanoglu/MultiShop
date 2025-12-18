021693923b878b5b4d172b945888d42fb15f0c87
BarisElvanoglu <elvanoglubaris12@gmail.com>
2025-12-18 15:11:59 +0300

    Adresleri listeleme ve Id ye göre getirme Handle methodları

---
+++ Order/Core/MultiShop.Order.Application/Features/CQRS/Handler/AddressHandler/GetAddressByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries;
+using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShot.Order.Applicatinon.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handler.AddressHandler
+{
+    public class GetAddressByIdQueryHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public GetAddressByIdQueryHandler(IRepository<Address> repository)
+        {
+            this._repository = repository;
+        }
+        public async Task<GetAddressByIdQueryResult> Handle(GetAddressByIdQuery query)   
+        {
+            var values = await _repository.GetByIdAsync(query.Id); 
+            return new GetAddressByIdQueryResult
+            {
+                AddressId = values.AddressId,
+                UserId = values.UserId,
+                City = values.City,
+                District = values.District,
+                Detail = values.Detail
+            };  
+        }
+    }
+}
---
+++ Order/Core/MultiShop.Order.Application/Features/CQRS/Handler/AddressHandler/GetAddressQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShot.Order.Applicatinon.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handler.AddressHandler
+{
+    public class GetAddressQueryHandler
+    {
+        private readonly IRepository<Address> _repository;  
+        public GetAddressQueryHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetAddressQueryResul>> Handle()
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetAddressQueryResul
+            {
+                AddressId = x.AddressId,
+                UserId = x.UserId,
+                City = x.City,
+                District = x.District,
+                Detail = x.Detail
+            }).ToList();
+        }   
+    }
+}
