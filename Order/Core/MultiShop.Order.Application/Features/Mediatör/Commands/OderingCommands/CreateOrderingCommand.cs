7b2b40b6536e219b5632947829e2c77609c94098
BarisElvanoglu <elvanoglubaris12@gmail.com>
2025-12-22 17:43:46 +0300

    Mediatr Ordering sınıfı işlemleri

---
+++ Order/Core/MultiShop.Order.Application/Features/Mediatör/Commands/OderingCommands/CreateOrderingCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediatör.Commands.OderingCommands
+{
+    public class CreateOrderingCommand
+    {
+        public string UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
+}
---
+++ Order/Core/MultiShop.Order.Application/Features/Mediatör/Commands/OderingCommands/RemoveOrderingRequest.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.Mediatör.Commands.OderingCommands
+{
+    public class RemoveOrderingRequest : IRequest
+    {
+        public int Id { get; set; }
+        public RemoveOrderingRequest(int id)
+        {
+            Id = id;
+        }   
+    }
+}
