a5c41e921826852767730535c7246cf8db41e680
BarisElvanoglu <elvanoglubaris12@gmail.com>
2025-12-22 11:21:29 +0300

    OrderDetailHanlerların tamamlanması CQRS

---
+++ Order/Core/MultiShop.Order.Application/Features/CQRS/Handler/OrderDetailHandlers/UpdateOrderDetailQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MultiShop.Order.Application.Interfaces;
+using MultiShot.Order.Applicatinon.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handler.OrderDetailHandlers
+{
+    public class UpdateOrderDetailQueryHandler
+    {
+        private readonly IRepository<OrderDetail> _addressRepository;
+
+        public UpdateOrderDetailQueryHandler(IRepository<OrderDetail> addressRepository)
+        {
+            _addressRepository = addressRepository;
+        }
+
+        public async Task Handle(UpdateOrderDetailCommands command)
+        {
+            var values = await _addressRepository.GetByIdAsync(command.OrderDetailId);
+            values.OrderingId = command.OrderingId;
+            values.ProductId = command.ProductId;
+            values.ProductPrice = command.ProductPrice;
+            values.ProductName = command.ProductName;
+            values.ProductAmount = command.ProductAmount;
+            values.ProductTotalPrice = command.ProductTotalPrice;
+            await _addressRepository.UpdateAsync(values);
+        }
+    }
+}
