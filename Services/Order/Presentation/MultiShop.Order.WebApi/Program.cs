8c048791c338e2417fa9f3d0f101264861d77e9d
BarisElvanoglu <elvanoglubaris12@gmail.com>
2025-12-22 16:38:24 +0300

    CQRS Registiration

--- Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MultiShop.Order.WebApi/Program.cs
@@ -1,6 +1,20 @@
+using MultiShop.Order.Application.Features.CQRS.Handler.AddressHandler;
+using MultiShop.Order.Application.Features.CQRS.Handler.OrderDetailHandlers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<GetAddressQueryHandler>();
+builder.Services.AddScoped<GetAddressByIdQueryHandler>();
+builder.Services.AddScoped<CreateAddressCommandHandler>();
+builder.Services.AddScoped<RemoveAddressCommandHandler>();
+builder.Services.AddScoped<UpdateAddressCommandHandler>();
+
+builder.Services.AddScoped<GetOrderDetailQueryHandler>();
+builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
+builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
+builder.Services.AddScoped<RemoveOrderDetailQueryHandler>();
+builder.Services.AddScoped<UpdateOrderDetailQueryHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
