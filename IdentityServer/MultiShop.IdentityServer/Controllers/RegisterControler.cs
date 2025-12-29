d995fedeb5a4ea234aba7c8e384228e98703bcf4
BarisElvanoglu <elvanoglubaris12@gmail.com>
2025-12-29 13:47:21 +0300

    User register dto ve registers controler

---
+++ IdentityServer/MultiShop.IdentityServer/Controllers/RegisterControler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.IdentityServer.Dtos;
+using MultiShop.IdentityServer.Models;
+using System.Threading.Tasks;
+
+namespace MultiShop.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterControler : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegisterControler(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }   
+        
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegisterDto userRegisterDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = userRegisterDto.Username,
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                SurName = userRegisterDto.Surname
+            };
+            var result = await _userManager.CreateAsync(user, userRegisterDto.Password);
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            return BadRequest(result.Errors);
+        }
+}
