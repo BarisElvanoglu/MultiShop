c4cdf6b303eb0b5b7958610b87bce7d3134e0f99
BarisElvanoglu <elvanoglubaris12@gmail.com>
2026-01-01 17:09:17 +0300

    İdentity visitor client

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -15,7 +15,9 @@ namespace MultiShop.IdentityServer
     {
         public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
         {
-            new ApiResource("ResourceCatalog"){Scopes={"CatalogFullPermission"}}
+            new ApiResource("ResourceCatalog"){Scopes={"CatalogFullPermission"}},
+            new ApiResource("ResourceDiscount"){Scopes={"DiscountFullPermission"}},
+            new ApiResource("ResourceOrder"){Scopes={"OrderFullPermission"}}
 
         };
 
@@ -30,7 +32,29 @@ namespace MultiShop.IdentityServer
         public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
         {
             new ApiScope("CatalogFullPermission, Full authority for catalog operation"),
-            new ApiScope("CatalogFullPermission, Reading authory for catalog operations")
+            new ApiScope("CatalogFullPermission, Reading authory for catalog operations"),
+            new ApiScope("DiscountFullPermission, Full authority for discount operation"),
+            new ApiScope("OrderFullPermission, Full authority for order operation")
+        };
+
+        new public static IEnumerable<Client> Clients => new Client[]
+        {
+            new Client
+            {
+                ClientId="MultiShopVisitorId",
+                ClientName="Multi Shop visitor user",
+                AllowedGrantTypes=GrantTypes.ClientCredentials,
+                ClientSecrets={ new Secret("secret".Sha256()) },
+                AllowedScopes={
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.Email,
+                    "CatalogFullPermission",
+                    "DiscountFullPermission",
+                    "OrderFullPermission"
+                }
+
+            }
         };
 
     }
