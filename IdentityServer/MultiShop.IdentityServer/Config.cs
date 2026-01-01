df50ec142ad9a1861add0f68870fd3585e474c0c
BarisElvanoglu <elvanoglubaris12@gmail.com>
2026-01-01 18:51:37 +0300

    Admin ve Manager kullanıcı yetkileri

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -7,6 +7,7 @@
 using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
+using System.Data.Common;
 using static System.Formats.Asn1.AsnWriter;
 
 namespace MultiShop.IdentityServer
@@ -37,24 +38,44 @@ namespace MultiShop.IdentityServer
             new ApiScope("OrderFullPermission, Full authority for order operation")
         };
 
-        new public static IEnumerable<Client> Clients => new Client[]
-        {
+        public static IEnumerable<Client> Clients => new Client[]
+        {   //visitor Client
             new Client
             {
                 ClientId="MultiShopVisitorId",
                 ClientName="Multi Shop visitor user",
                 AllowedGrantTypes=GrantTypes.ClientCredentials,
-                ClientSecrets={ new Secret("secret".Sha256()) },
-                AllowedScopes={
-                    IdentityServerConstants.StandardScopes.OpenId,
-                    IdentityServerConstants.StandardScopes.Profile,
-                    IdentityServerConstants.StandardScopes.Email,
-                    "CatalogFullPermission",
-                    "DiscountFullPermission",
-                    "OrderFullPermission"
-                }
+                ClientSecrets={ new Secret("multishopsecret".Sha256()) },
+                AllowedScopes={"CatalogReadPermission"}
+
+            },
+            //Manager
+            new Client
+            {
+                ClientId="MultiShopManagerId",
+                ClientName="Multi Shop Manager User",
+                AllowedGrantTypes=GrantTypes.ClientCredentials,
+                ClientSecrets={ new Secret("multishopsecret".Sha256()) },
+                AllowedScopes={ "CatalogReadPermission","CatalogFullPermission" }
+
+            },
+            //Admin
+            new Client
+            {
+            ClientId = "MultiShopAdminId",
+            ClientName = "Multi Shop Admin User",
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            ClientSecrets = { new Secret("multishopsecret".Sha256()) },
+            AllowedScopes = { "CatalogFullPermission", "DiscountFullPermission", "OrderFullPermission","CatalogReadPermission",
+                IdentityServerConstants.LocalApi.ScopeName,
+                IdentityServerConstants.StandardScopes.Email,
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile,
+                },
+             AccessTokenLifetime=600
 
             }
+
         };
 
     }
