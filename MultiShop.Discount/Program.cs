7e0eea2aca83f66e7477195fef6c7b21cec11e58
BarisElvanoglu <elvanoglubaris12@gmail.com>
2026-01-07 16:18:00 +0300

    Discount Microservice ini Token bile koruma

--- MultiShop.Discount/Program.cs
+++ MultiShop.Discount/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using MultiShop.Discount.Context;
 using MultiShop.Discount.Services;
 
@@ -5,6 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "ResourceDiscount";
+    options.RequireHttpsMetadata = false;
+});
 
 builder.Services.AddTransient<DapperContext>();
 builder.Services.AddTransient<IDiscountService, DiscountService>(); 
@@ -25,7 +32,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
