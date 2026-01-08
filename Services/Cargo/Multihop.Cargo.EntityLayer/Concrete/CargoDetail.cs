486950e0367c6194400f7f0747d5ba4970a05703
BarisElvanoglu <elvanoglubaris12@gmail.com>
2026-01-08 12:37:59 +0300

    Kargo entity fix

--- Services/Cargo/Multihop.Cargo.EntityLayer/Concrete/CargoDetail.cs
+++ Services/Cargo/Multihop.Cargo.EntityLayer/Concrete/CargoDetail.cs
@@ -10,10 +10,9 @@ namespace Multihop.Cargo.EntityLayer.Concrete
     public class CargoDetail
     {
         public int CargoDetailId { get; set; }
-        public int SenderCustomer { get; set; }
+        public string SenderCustomer { get; set; }
         public string ReceiverCustomer { get; set; }
         public int Barcode { get; set; }
-
         public int CargoCompanyId { get; set; }
         public CargoCompany CargoCompany { get; set; }
     }
