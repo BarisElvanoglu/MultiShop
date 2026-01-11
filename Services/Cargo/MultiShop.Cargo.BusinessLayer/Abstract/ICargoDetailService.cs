c2c0356c45576dd69d34198d65aa84e4ef00683c
BarisElvanoglu <elvanoglubaris12@gmail.com>
2026-01-11 16:29:11 +0300

    Business katmanı Interfaceler

---
+++ Services/Cargo/MultiShop.Cargo.BusinessLayer/Abstract/ICargoDetailService.cs
@@ -0,0 +1,14 @@
+﻿using Multihop.Cargo.EntityLayer.Concrete;
+using MultiShop.Cargo.DataAccsessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.BusinessLayer.Abstract
+{
+    public interface ICargoDetailService: IGenericService<CargoDetail> 
+    {
+    }
+}
