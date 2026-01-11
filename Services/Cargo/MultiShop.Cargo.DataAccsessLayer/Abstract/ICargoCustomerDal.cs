1cd0b3a0485db59cd0f562bf13e87baa4919082d
BarisElvanoglu <elvanoglubaris12@gmail.com>
2026-01-11 14:44:12 +0300

    CargoGeneric dal ve interfaceler

---
+++ Services/Cargo/MultiShop.Cargo.DataAccsessLayer/Abstract/ICargoCustomerDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Multihop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.DataAccsessLayer.Abstract
+{
+    public interface ICargoCustomerDal:IGenericDal<CargoCustomer>
+    {
+    }
+}
---
+++ Services/Cargo/MultiShop.Cargo.DataAccsessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccsessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T obj);
+        void Delete(T obj);
+        void Update(T obj);
+        List<T> GetAll();
+        T GetById(int id);
+    }
+}
