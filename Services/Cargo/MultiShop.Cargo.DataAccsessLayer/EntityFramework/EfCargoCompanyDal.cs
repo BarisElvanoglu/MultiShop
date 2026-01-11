207e96d4cca871c33a0002187bd4a4df7f0f169d
BarisElvanoglu <elvanoglubaris12@gmail.com>
2026-01-11 15:21:15 +0300

    Entityframework sınıları

---
+++ Services/Cargo/MultiShop.Cargo.DataAccsessLayer/EntityFramework/EfCargoCompanyDal.cs
@@ -0,0 +1,19 @@
+﻿using Multihop.Cargo.EntityLayer.Concrete;
+using MultiShop.Cargo.DataAccsessLayer.Abstract;
+using MultiShop.Cargo.DataAccsessLayer.Concrete;
+using MultiShop.Cargo.DataAccsessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccsessLayer.EntityFramework
+{
+    public class EfCargoCompanyDal : GenericRepository<CargoCompany>, ICargoCompanyDal
+    {
+        public EfCargoCompanyDal(CargoContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Services/Cargo/MultiShop.Cargo.DataAccsessLayer/EntityFramework/EfCargoOperationDal.cs
@@ -0,0 +1,20 @@
+﻿using Multihop.Cargo.EntityLayer.Concrete;
+using MultiShop.Cargo.DataAccsessLayer.Abstract;
+using MultiShop.Cargo.DataAccsessLayer.Concrete;
+using MultiShop.Cargo.DataAccsessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccsessLayer.EntityFramework
+{
+    public class EfCargoOperationDal: GenericRepository<CargoOperation>, ICargoOperationDal
+    {
+        public EfCargoOperationDal(CargoContext context) : base(context)
+        {
+        }   
+
+    }
+}
